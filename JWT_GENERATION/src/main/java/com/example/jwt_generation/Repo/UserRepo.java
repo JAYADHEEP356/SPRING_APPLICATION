0567cf5061743341c030132f31dfa07522fe0c9a
JAYADHEEP356 <123255798+JAYADHEEP356@users.noreply.github.com>
2025-04-03 23:18:29 +0530

    Delete JWT_GENERATION directory

--- JWT_GENERATION/src/main/java/com/example/jwt_generation/Repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.jwt_generation.Repo;
-
-import com.example.jwt_generation.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,String>{
-
-}
